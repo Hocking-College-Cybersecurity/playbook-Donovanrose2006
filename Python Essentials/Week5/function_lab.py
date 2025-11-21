b02123bd2cf42571c5679c3c9ca0960db7a45a70
Donovanrose2006 <donovanrose2006@gmail.com>
2025-11-21 05:04:39 -0500

    fixed and streamlined function_lab.py

--- Python Essentials/Week5/function_lab.py
+++ Python Essentials/Week5/function_lab.py
@@ -11,34 +11,43 @@ while(True): #Creates a simple loop
     print("1. Days to seconds")
     print("2. Hours to seconds")
     print("3. Minutes to seconds")
-    print("4. Exit")
-    choice = input("Choose an option (1, 2, 3, or 4): ")#Awaits user input
+    print("4. Days, hours, and minutes to seconds")
+    print("5. Exit")
+    choice = input("Choose an option (1-5): ")#Awaits user input
 
     if choice == "1":
-        value = input("Enter number of days: ")#Waits for days to convert to seconds
         try:
-            days = int(value)
+            days = int(input("Enter number of days: "))
             print(str(days) + " days = " + str(days_to_seconds(days)) + " seconds")#Prints the entered amount of days and the seconds in them
         except:
             print("Not a valid number.")
 
     elif choice == "2":
-        value = input("Enter number of hours: ")#Same thing with hours
         try:
-            hours = int(value)
+            hours = int(input("Enter number of hours: "))
             print(str(hours) + " hours = " + str(hours_to_seconds(hours)) + " seconds")#Prints hours and the seconds in them
         except:
             print("Not a valid number.")
 
     elif choice == "3":
-        value = input("Enter number of minutes: ")#Same with minutes
         try:
-            minutes = int(value)
+            minutes = int(input("Enter number of minutes: "))
             print(str(minutes) + " minutes = " + str(minutes_to_seconds(minutes)) + " seconds")#Prints minutes and the seconds in them
         except:
             print("Not a valid number.")
     elif choice == "4":
+        try:
+            days = int(input("Enter number of days: "))
+            hours = int(input("Enter number of hours: "))
+            minutes = int(input("Enter number of minutes: "))
+            
+            total_seconds = days_to_seconds(days) + hours_to_seconds(hours) + minutes_to_seconds(minutes) #Adds the totals
+            print(str(days) + " days, " + str(hours) + " hours, and " + str(minutes) + " minutes = " + str(total_seconds) + " seconds") #Gives the user all the values they entered converted into seconds added together
+        except:
+            print("Please enter valid numbers only.")
+
+    elif choice == "5":
         print("Goodbye!")
         break #Breaks the simple loop
     else:
-        print("Invalid choice. Please select 1, 2, 3, or 4.")
+        print("Invalid choice. Please select an option (1-5).")
